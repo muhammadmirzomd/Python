a7ecc33389f74c34999983132af8b5dcf2599592
muhammadmirzomd <muhammadmirzomd@gmail.com>
2025-05-08 16:06:50 +0500

    New commit

---
+++ 13-dars OOP/homework.py
@@ -0,0 +1,48 @@
+"""
+1-misol
+"""
+# class Talaba:
+#     def __init__(self, ism, yosh, kurs, institut, yonalish):
+#         self.ism = ism
+#         self.yosh = yosh
+#         self.kurs = kurs
+#         self.institut = institut
+#         self.yonalish = yonalish
+#
+#     def tanishtir(self):
+#         print(f"Men {self.ism}, {self.yosh} yoshdaman.")
+#         print(f"{self.institut} institutida {self.kurs}-kursda o‘qiyman.")
+#         print(f"Yo‘nalishim: {self.yonalish}")
+#
+# ism = input("Ismingizni kiriting: ")
+# yosh = int(input("Yoshingizni kiriting: "))
+# kurs = int(input("Kursingizni kiriting: "))
+# institut = input("Institut nomini kiriting: ")
+# yonalish = input("Yo‘nalishingizni kiriting: ")
+#
+# talaba1 = Talaba(ism, yosh, kurs, institut, yonalish)
+# talaba1.tanishtir()
+
+"""
+2-misol
+"""
+# class Kitob:
+#     def __init__(self, nomi, muallif, janr, sahifa_soni, yil):
+#         self.nomi = nomi
+#         self.muallif = muallif
+#         self.janr = janr
+#         self.sahifa = sahifa_soni
+#         self.yil = yil
+#
+#     def info(self):
+#         print(f"Kitob: {self.nomi}, Muallif: {self.muallif}")
+#         print(f"Janr: {self.janr}, Sahifa: {self.sahifa}, Yili: {self.yil}")
+#
+# nomi = input("Kitob nomi: ")
+# muallif = input("Muallif: ")
+# janr = input("Janr: ")
+# sahifa = int(input("Sahifa soni: "))
+# yil = int(input("Chop etilgan yil: "))
+#
+# k = Kitob(nomi, muallif, janr, sahifa, yil)
+# k.info()
