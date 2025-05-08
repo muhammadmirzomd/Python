d8106e4e223a0fcfbcedabc250288335b39c683f
muhammadmirzomd <muhammadmirzomd@gmail.com>
2025-05-08 16:11:37 +0500

    New commit

--- 13-dars OOP/homework.py
+++ 13-dars OOP/homework.py
@@ -46,3 +46,82 @@
 #
 # k = Kitob(nomi, muallif, janr, sahifa, yil)
 # k.info()
+
+"""
+3-misol
+"""
+
+# class Qabul:
+#     def __init__(self, bemor, shifokor, yonalish, sana, vaqti):
+#         self.bemor = bemor
+#         self.shifokor = shifokor
+#         self.yonalish = yonalish
+#         self.sana = sana
+#         self.vaqti = vaqti
+#
+#     def info(self):
+#         print(f"Bemor: {self.bemor}")
+#         print(f"Qabul: {self.shifokor} ({self.yonalish})")
+#         print(f"Sana: {self.sana}, Soat: {self.vaqti}")
+#
+# bemor = input("Bemor ismi: ")
+# shifokor = input("Shifokor ismi: ")
+# yonalish = input("Yo‘nalish (masalan, kardiolog): ")
+# sana = input("Qabul kuni: ")
+# vaqti = input("Soat: ")
+#
+# q = Qabul(bemor, shifokor, yonalish, sana, vaqti)
+# q.info()
+
+'''
+4-misol
+'''
+
+# class Dostavka:
+#     def __init__(self, ism, manzil, mahsulot, masofa, narx):
+#         self.ism = ism
+#         self.manzil = manzil
+#         self.mahsulot = mahsulot
+#         self.masofa = masofa
+#         self.narx = narx
+#
+#     def info(self):
+#         print(f"Buyurtmachi: {self.ism}")
+#         print(f"Manzil: {self.manzil}, Mahsulot: {self.mahsulot}")
+#         print(f"Masofa: {self.masofa} km, Narx: {self.narx} so‘m")
+#
+# ism = input("Ismingiz: ")
+# manzil = input("Manzilingiz: ")
+# mahsulot = input("Buyurtma qilgan mahsulot: ")
+# masofa = float(input("Masofa (km): "))
+# narx = int(input("Dostavka narxi: "))
+#
+# d = Dostavka(ism, manzil, mahsulot, masofa, narx)
+# d.info()
+
+"""
+5-misol
+"""
+
+# class OnlineDars:
+#     def __init__(self, fan, oqituvchi, boshlanish_vaqti, davomiylik, platforma):
+#         self.fan = fan
+#         self.oqituvchi = oqituvchi
+#         self.boshlanish = boshlanish_vaqti
+#         self.davomiylik = davomiylik
+#         self.platforma = platforma
+#
+#     def info(self):
+#         print(f"Fan: {self.fan}")
+#         print(f"O‘qituvchi: {self.oqituvchi}")
+#         print(f"Boshlanish: {self.boshlanish} | Davomiylik: {self.davomiylik} daqiqa")
+#         print(f"Dars platformasi: {self.platforma}")
+#
+# fan = input("Fan nomi: ")
+# oqituvchi = input("O‘qituvchi ismi: ")
+# boshlanish = input("Boshlanish vaqti: ")
+# davomiylik = int(input("Davomiylik (daqiqa): "))
+# platforma = input("Platforma: ")
+#
+# d1 = OnlineDars(fan, oqituvchi, boshlanish, davomiylik, platforma)
+# d1.info()
