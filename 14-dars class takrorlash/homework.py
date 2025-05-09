c8b1a8672443aa247637532efc1c165d3b65cff8
muhammadmirzomd <muhammadmirzomd@gmail.com>
2025-05-09 22:19:28 +0500

    New commit

---
+++ 14-dars class takrorlash/homework.py
@@ -0,0 +1,39 @@
+"""
+1-misol
+"""
+# class Kitob:
+#     def __init__(self, nomi, muallif, janr, yil, betlar, nashriyot):
+#         self.nomi = nomi
+#         self.muallif = muallif
+#         self.janr = janr
+#         self.yil = yil
+#         self.betlar = betlar
+#         self.nashriyot = nashriyot
+#
+#     def malumotlar(self):
+#         return f"{self.nomi} ({self.janr}) - {self.muallif}, {self.yil} yil"
+#
+#     def muallif_nomi(self):
+#         return self.muallif
+#
+#     def kitob_betlari(self):
+#         return self.betlar > 300
+#
+#     def haqida(self):
+#         return f"{self.nomi} - {self.betlar} bet"
+#
+#     def kitobning_yili(self):
+#         return self.yil < 2000
+#
+#     def nashriyot_nomi(self):
+#         return self.nashriyot
+#
+# k = Kitob("O‘tkan kunlar", "Abdulla Qodiriy", "Roman", 1926, 350, "G'afur G'ulom")
+# print(k.malumotlar())
+
+
+
+"""
+3-misol
+"""
+
