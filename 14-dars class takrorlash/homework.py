02b67e1859714e033f20b07093c578b6129bab50
muhammadmirzomd <muhammadmirzomd@gmail.com>
2025-05-09 22:19:46 +0500

    New commit

--- 14-dars class takrorlash/homework.py
+++ 14-dars class takrorlash/homework.py
@@ -31,7 +31,39 @@
 # k = Kitob("O‘tkan kunlar", "Abdulla Qodiriy", "Roman", 1926, 350, "G'afur G'ulom")
 # print(k.malumotlar())
 
-
+"""
+2-misol
+"""
+# class Talaba:
+#     def __init__(self, ism, familiya, fakultet, kurs, id_raqam, email):
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.fakultet = fakultet
+#         self.kurs = kurs
+#         self.id_raqam = id_raqam
+#         self.email = email
+#
+#     def toliq_ism(self):
+#         return f"{self.ism} {self.familiya}"
+#
+#     def kurs_darajasi(self):
+#         return f"{self.kurs}-kurs"
+#
+#     def kontakt(self):
+#         return self.email
+#
+#     def talaba_id(self):
+#         return self.id_raqam
+#
+#     def fakultet_nomi(self):
+#         return self.fakultet
+#
+#     def nmadr(self):
+#         return f"{self.toliq_ism()} - {self.fakultet}, {self.kurs}-kurs"
+#
+# t = Talaba("Kmdr", "Kmdurov", "Axborot texnologiyalari", 3, 10458, "dilshod@gmail.com")
+# print(t.nmadr())
+# print(t.kontakt())
 
 """
 3-misol
